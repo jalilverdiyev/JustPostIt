409d5db830231efe13c0e64f5383049a43303d52
jalilverdiyev <calilverdiyev2@gmail.com>
2022-09-16 04:14:19 +0400

    added alert when user tries to create more than one account with same email

---
+++ JustPostItDesktop/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+namespace JustPostItDesktop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
---
+++ JustPostItDesktop/Views/LoginPage.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace JustPostItDesktop.Views
+{
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
